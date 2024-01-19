f2d89179c86c87d6e108656e1f0d191d3749f4f9
Rishabh Sahu <rishabhsahu13873@gmail.com>
2024-01-19 20:07:39 +0530

    digital clock

---
+++ index.js
@@ -0,0 +1,12 @@
+var week= ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"]
+
+function update(){
+    var d=new Date;
+    document.querySelector(".daytext").innerHTML=week[d.getDay()];
+    document.querySelector(".datetext").innerHTML=d.getDate()+"-"+d.getMonth()+1+"-"+d.getFullYear();
+
+    document.querySelector(".timetext").innerHTML=d.getHours()+" \xa0 : \xa0 "+d.getMinutes()+" \xa0 : \xa0"+d.getSeconds();
+
+}
+
+setInterval(update,1000);
