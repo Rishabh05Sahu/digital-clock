69f55760815df83d50f9895377e61bd7df322ad0
Rishabh05Sahu <111513659+Rishabh05Sahu@users.noreply.github.com>
2024-03-08 13:57:30 +0530

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var week= ["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday
 
 function update(){
     var d=new Date;
-    document.querySelector(".daytext").innerHTML=week[d.getDay()];
+    document.querySelector(".daytext").innerHTML=week[d.getDay()+1];
     document.querySelector(".datetext").innerHTML=d.getDate()+"-"+d.getMonth()+1+"-"+d.getFullYear();
 
     document.querySelector(".timetext").innerHTML=d.getHours()+" \xa0 : \xa0 "+d.getMinutes()+" \xa0 : \xa0"+d.getSeconds();
